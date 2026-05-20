7989bf8fd33d5538ca4b009b7f727183c94ddcba
NimmraTariq <nimra369tariq@gmail.com>
2026-05-20 11:23:05 +0700

    auto: Implement AuthService and Profile entity

--- src/main/java/com/deploy/pertamuan11/model/Profile.java
+++ src/main/java/com/deploy/pertamuan11/model/Profile.java
@@ -1,4 +1,26 @@
 package com.deploy.pertamuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@Table(name = "profiles")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Profile {
+	@Id
+	private String id;
+
+	private String nama;
+
+	private String alamat;
+
+	@OneToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 }
